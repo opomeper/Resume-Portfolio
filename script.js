a3e867da9a98fe805479dd7cd3043d8adc53b321
opomeper <saurojoemer14@gmail.com>
2025-09-17 16:13:05 +0800

    Add new Skills, remove unnecessray codes/comment, modified Recommendations

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function addRecommendation() {
   // Get the message of the new recommendation
   let recommendation = document.getElementById("new_recommendation");
+  let name = document.getElementById("name");
   // If the user has left a recommendation, display a pop-up
   if (recommendation.value != null && recommendation.value.trim() != "") {
     console.log("New recommendation added");
@@ -9,12 +10,13 @@ function addRecommendation() {
     // Create a new 'recommendation' element and set it's value to the user's message
     var element = document.createElement("div");
     element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>" + "<br/>" + "-" + name.value;
     // Add this element to the end of the list of recommendations
     document.getElementById("all_recommendations").appendChild(element); 
     
     // Reset the value of the textarea
     recommendation.value = "";
+    name.value = "";
     showPopup(true);
   }
   else {
